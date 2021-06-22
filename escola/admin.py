06e1d1b69ae3963d00bb26e42bb4fe1468789364
Marcos Souza <mrcsz.m@gmail.com>
2021-06-22 13:49:55 -0300

    admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Aluno, Curso
+
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+
+admin.site.register(Aluno, AlunoAdmin)
+
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
+
+
+admin.site.register(Curso, CursoAdmin)
