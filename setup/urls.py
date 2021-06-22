20a8788fcea3c3c282acbad799a1ea5be2965990
Marcos Souza <mrcsz.m@gmail.com>
2021-06-22 14:10:43 -0300

    urls routers restframework

--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from escola.views import alunos
+from escola.views import AlunosViewSet, CursosViewSet
 
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='alunos')
+router.register('cursos', CursosViewSet, basename='cursos')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos)
-
+    path('', include(router.urls)),
 ]
